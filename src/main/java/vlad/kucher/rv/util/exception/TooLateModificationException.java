3618347a31c2032ea5e24ee4c291535a0baa3e77
Vlad <vlad.kucher.92@gmail.com>
2017-10-07 18:16:53 +0300

    exceptions response status

--- src/main/java/vlad/kucher/rv/util/exception/TooLateModificationException.java
+++ src/main/java/vlad/kucher/rv/util/exception/TooLateModificationException.java
@@ -1,5 +1,9 @@
 package vlad.kucher.rv.util.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.CONFLICT, reason = "Vote can't be changed after 11:00")  // 409
 public class TooLateModificationException extends RuntimeException{
     public TooLateModificationException(String message) {
         super(message);
