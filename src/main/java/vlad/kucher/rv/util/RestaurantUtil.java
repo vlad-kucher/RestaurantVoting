49f4a3e471887dfaaa565d12b093d280dd015a3d
Vlad <vlad.kucher.92@gmail.com>
2017-10-15 17:46:20 +0300

    stream api

--- src/main/java/vlad/kucher/rv/util/RestaurantUtil.java
+++ src/main/java/vlad/kucher/rv/util/RestaurantUtil.java
@@ -3,21 +3,15 @@ package vlad.kucher.rv.util;
 import vlad.kucher.rv.model.Menu;
 import vlad.kucher.rv.to.RestaurantTo;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RestaurantUtil {
     private RestaurantUtil() {
     }
 
     public static List<RestaurantTo> getTo(List<Menu> menus){
-        List<RestaurantTo> toList = new ArrayList<>();
-
-        for (Menu m : menus) {
-            toList.add(createTo(m));
-        }
-
-        return toList;
+        return menus.stream().map(RestaurantUtil::createTo).collect(Collectors.toList());
     }
 
     public static RestaurantTo createTo(Menu menu){
